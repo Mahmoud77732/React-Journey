b9c3da552818465eca3e2847c6eb6ef392f8408e
mahmoud.hegazy <mahmoud.hegazy@3ddx.com>
2026-01-06 15:29:39 +0200

    React components, props

---
+++ 01-starting-project/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import reactImg from '../assets/react-core-concepts.png';
+
+const reactDesc = ['Fundmental', 'Crucial', 'Core'];
+
+function genRandomNum(max) {
+  return Math.floor(Math.random() * (max+1));
+}
+
+export default function Header(){
+  const description = reactDesc[genRandomNum(2)]; // array: 0, 1, 2
+  return (
+      <header>
+        {/* <img src="src/assets/react-core-concepts.png" alt="Stylized atom" /> */}
+        <img src={reactImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+  );
+}
